e3648ddbb6f043b09cad475d3cfad8e6014a32ca
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2025-03-14 13:16:05 -0500

    Add main layout with login verification

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,50 @@
+import { Stack, useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import store, { login } from '../redux/store';
+import { useEffect, useState } from 'react';
+import * as SecureStore from 'expo-secure-store';
+import { MenuProvider } from 'react-native-popup-menu';
+
+function MainLayout() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const userData = await SecureStore.getItemAsync('userData');
+      if (userData) {
+        dispatch(login());
+      }
+      setLoading(false);
+    };
+    checkLoginStatus();
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!loading && !isLoggedIn) {
+      router.replace('/login');
+    }
+  }, [isLoggedIn, loading, router]);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="login" />
+      </Stack>
+    </SafeAreaView>
+  );
+}
+
+export default function Layout() {
+  return (
+    <Provider store={store}>
+      <MenuProvider>
+        <MainLayout />
+      </MenuProvider>
+    </Provider>
+  );
+}
